ed0a975bb68f9b59ad9b6cf6aa45f9adce855c09
vabeledo <abeledovictor@gmail.com>
2023-08-28 15:20:28 +0200

    build static

--- src/app/page.js
+++ src/app/page.js
@@ -221,7 +221,7 @@ export default function Home() {
                       <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
                   </svg> 
                   <blockquote>
-                      <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">"We have been working with OMGSeo for nearly 6 months now. They will get things done on time. They have always been realistic with their expectations and delivered as promised. Pricing is transparent and at par with market rates."</p>
+                      <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">&quot;We have been working with OMGSeo for nearly 6 months now. They will get things done on time. They have always been realistic with their expectations and delivered as promised. Pricing is transparent and at par with market rates.&quot;</p>
                   </blockquote>
                   <figcaption className="flex items-center justify-center mt-6 space-x-3">
                       <img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture" />
